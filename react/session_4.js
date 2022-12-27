8ed298d2e82a6ed8f64c06313799874382f3620e
Admin <kavyak6999@gmail.com>
2022-12-27 08:39:11 +0530

    template literals

--- react/session_4.js
+++ react/session_4.js
@@ -16,5 +16,23 @@ getName.apply(obj,[22,33]);
 var fun=getName.bind(obj,222,333);
 fun();
 
+//ES6 features
+// function main()
+// {
+//     var a = 1
+//     if(a===1)
+//     {
+//         let b=2;
+//         console.log(a+b);
+//     }
+//     console.log(a+b);
+// }
+// main();
+
+//template literals
+var x = 5;
+var str = "678";
+//console.log(x+str);
+console.log(`${x}${str}`);
 
 
